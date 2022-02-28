abc1ba7851e01ba8940c463195156f58ed6a25cd
Well5a <marcelfrederik@aol.de>
2022-02-28 08:51:05 +0100

    initialize spring boot project

---
+++ src/main/java/ust/tad/analysismanager/AnalysisManagerApplication.java
@@ -0,0 +1,13 @@
+package ust.tad.analysismanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalysisManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalysisManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ust/tad/analysismanager/AnalysisManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ust.tad.analysismanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalysisManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
