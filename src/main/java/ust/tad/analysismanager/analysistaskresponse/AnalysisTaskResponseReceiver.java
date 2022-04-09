f3db612fa864d7f25422bc58ebd0c8b386abcccd
Well5a <marcelfrederik@aol.de>
2022-04-09 12:33:11 +0200

    improved receiving responses

--- src/main/java/ust/tad/analysismanager/analysistaskresponse/AnalysisTaskResponseReceiver.java
+++ src/main/java/ust/tad/analysismanager/analysistaskresponse/AnalysisTaskResponseReceiver.java
@@ -1,17 +1,56 @@
 package ust.tad.analysismanager.analysistaskresponse;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class AnalysisTaskResponseReceiver {
+  
+    @Autowired
+    private MessageConverter jsonMessageConverter;
 
     private static final Logger LOG =
       LoggerFactory.getLogger(AnalysisTaskResponseReceiver.class);
 
     @RabbitListener(queues = "#{analysisTaskResponseQueue.name}")
-    public void receiveAnalysisTaskResponse(AnalysisTaskResponse analysisTaskResponse) {
-        LOG.info("received AnalysisTaskResponse: " + analysisTaskResponse.toString());
+    public void receive(Message message) {
+      try {
+        switch (message.getMessageProperties().getHeader("formatIndicator").toString()) {
+            case "AnalysisTaskResponse":
+                receiveAnalysisTaskResponse(message);
+                break;
+            case "EmbeddedDeploymentModelAnalysisRequest":
+                receiveEmbeddedDeploymentModelAnalysisRequest(message);
+                break;        
+            default:
+                break;
+        }   
+      } catch (NullPointerException e) {
+          e.printStackTrace();
+      }
+    }
+
+    private void receiveAnalysisTaskResponse(Message message) {
+      ObjectMapper mapper = new ObjectMapper();
+        
+      AnalysisTaskResponse analysisTaskResponse = mapper.convertValue(
+          jsonMessageConverter.fromMessage(message), 
+          AnalysisTaskResponse.class);
+      LOG.info("received AnalysisTaskResponse: " + analysisTaskResponse.toString());
+    }
+
+    private void receiveEmbeddedDeploymentModelAnalysisRequest(Message message) {
+      ObjectMapper mapper = new ObjectMapper();
+        
+      EmbeddedDeploymentModelAnalysisRequest embeddedDeploymentModelAnalysisRequest = mapper.convertValue(
+          jsonMessageConverter.fromMessage(message), 
+          EmbeddedDeploymentModelAnalysisRequest.class);
+      LOG.info("received AnalysisTaskResponse: " + embeddedDeploymentModelAnalysisRequest.toString());
     }
     
 }
